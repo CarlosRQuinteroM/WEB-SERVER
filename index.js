a65dd3fb52b51d2f6f4064065184b930274899cb
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2024-02-01 22:31:22 +0100

    Create a Basic CRUD

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.clear();
 import express from "express";
-const PORT = 3000;
+import dotenv from "dotenv";
+
+import { USER_DB } from "./Bddb.js";
+
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
 
 const expressApp = express();
 
@@ -8,12 +14,56 @@ expressApp.use(express.json());
 expressApp.use(express.text());
 
 //Obtener detalles de la cuenta.
+expressApp.get("/account/:guid", (req, res) => {
+  const { guid } = req.params;
+  const user = USER_DB.find((user) => user.guid === guid);
+  if (!user) return res.status(404).send();
 
+  res.send(user);
+});
 //Crear una cuenta nueva.
+expressApp.post("/account/", (req, res) => {
+  const { guid, name } = req.body;
+  if (!guid) {
+    return res.status(400).send();
+  }
 
-//Actualizar una cuenta.
+  const user = USER_DB.find((user) => user.guid === guid);
+  if (user) {
+    return res.status(409).send();
+  }
+
+  USER_DB.push({
+    guid,
+    name,
+  });
+});
+//Actualizar el nombre de  una cuenta.
+
+expressApp.path("/account/:guid", (req, res) => {
+  const { guid } = req.params;
+  const { name } = req.body;
+  if (!name) return res.status(400).send();
+
+  const user = USER_DB.find((user) => user.guid === guid);
+  if (!user) return res.status(404).send();
+
+  user.name = name;
+
+  return res.send();
+});
 
 //Eliminar una Cuenta.
+expressApp.delete("/account/:guid", (req, res) => {
+  const { guid } = req.params;
+  const userIndex = USER_DB.findIndex((user) => user.guid === guid);
+  if (userIndex === -1) return res.status(404).send();
+
+  USER_DB.splice(userIndex, 1);
+
+  return res.send();
+});
+
 expressApp.listen(PORT, () =>
   console.log(`servidor levantado en el puerto ${PORT}`)
 );
